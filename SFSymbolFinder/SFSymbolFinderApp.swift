05b253c8d83f5719c5587e80b808b31b1cbec3b5
Gleb Ustymenko <gleb.ustimenko@gmail.com>
2024-10-04 11:49:08 +0200

    Initial Commit

---
+++ SFSymbolFinder/SFSymbolFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolFinderApp.swift
+//  SFSymbolFinder
+//
+//  Created by Gleb Ustymenko on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
